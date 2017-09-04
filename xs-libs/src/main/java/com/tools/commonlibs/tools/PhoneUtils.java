176c95b388a23bbb3ac555b989d3bc7659a37e7e
panhui <13391545091@126.com>
2017-09-04 17:54:32 +0800

    渠道

--- xs-libs/src/main/java/com/tools/commonlibs/tools/PhoneUtils.java
+++ xs-libs/src/main/java/com/tools/commonlibs/tools/PhoneUtils.java
@@ -5,6 +5,7 @@ import android.app.ActivityManager;
 import android.app.ActivityManager.RunningTaskInfo;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -315,6 +316,7 @@ public class PhoneUtils {
 
     }
 
+
     /**
      * 获取应用的版本号
      *
@@ -394,6 +396,34 @@ public class PhoneUtils {
             LogUtils.error(e.getMessage(), e);
         }
         return ip;
+    }
+
+
+    /**
+     * 获取application中指定的meta-data。本例中，调用方法时key就是UMENG_CHANNEL
+     * @return 如果没有获取成功(没有对应值，或者异常)，则返回值为空
+     */
+    public static String getAppMetaData(Context ctx) {
+        if (ctx == null ) {
+            return null;
+        }
+        String resultData = null;
+        try {
+            PackageManager packageManager = ctx.getPackageManager();
+            if (packageManager != null) {
+                ApplicationInfo applicationInfo = packageManager.getApplicationInfo(ctx.getPackageName(), PackageManager.GET_META_DATA);
+                if (applicationInfo != null) {
+                    if (applicationInfo.metaData != null) {
+                        resultData = applicationInfo.metaData.getString("UMENG_CHANNEL");
+                    }
+                }
+
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return resultData;
     }
 
 }
