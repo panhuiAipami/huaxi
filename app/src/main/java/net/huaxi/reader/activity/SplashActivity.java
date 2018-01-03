ee4a97d3a12d214202e95811735573bfa5dfe037
dongyongjie <13673315715@163.com>
2018-01-03 16:06:10 +0800

    底部icon与启动页

--- app/src/main/java/net/huaxi/reader/activity/SplashActivity.java
+++ app/src/main/java/net/huaxi/reader/activity/SplashActivity.java
@@ -6,8 +6,10 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.spriteapp.booklibrary.util.FileHelper;
 import com.spriteapp.booklibrary.util.GlideUtils;
@@ -20,22 +22,26 @@ import static net.huaxi.reader.MainActivity.SEXTIME;
 
 public class SplashActivity extends AppCompatActivity {
     private ImageView splash;
+    private TextView skip_ad;
+    private int num = 3;
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            Intent intent;
+
             switch (msg.what) {
                 case 0:
-                    boolean sex_first = PreferenceHelper.getBoolean(SEXTIME, true);
-                    if (sex_first) {
-                        intent = new Intent(SplashActivity.this, SexActivity.class);
+                    handler.removeCallbacksAndMessages(null);
+                    isGotoSex();
+                    break;
+                case 1:
+                    if (num > 1) {
+                        num--;
+                        skip_ad.setText("跳过" + num);
+                        handler.sendEmptyMessageDelayed(1, 1000);
                     } else {
-                        intent = new Intent(SplashActivity.this, MainActivity.class);
-
+                        handler.sendEmptyMessageDelayed(0, 1000);
                     }
-                    startActivity(intent);
-                    finish();
                     break;
             }
         }
@@ -47,18 +53,39 @@ public class SplashActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
         setContentView(R.layout.activity_splash);
         splash = (ImageView) findViewById(R.id.splash);
+        skip_ad = (TextView) findViewById(R.id.skip_ad);
+        skip_ad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handler.sendEmptyMessage(0);
+            }
+        });
         try {
             Log.d("loadImage", "url==");
             String url = FileHelper.readObjectFromJsonFile(this, "start_page", String.class);
             if (url != null && !url.isEmpty() && net.huaxi.reader.utils.Util.isNetAvailable(this)) {
                 Log.d("loadImage", "url===" + url);
                 GlideUtils.loadImage(splash, url, this);
+//                skip_ad.setVisibility(View.VISIBLE);
 
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+    public void isGotoSex() {
+        Intent intent;
+        boolean sex_first = PreferenceHelper.getBoolean(SEXTIME, true);
+        if (sex_first) {
+            intent = new Intent(SplashActivity.this, SexActivity.class);
+        } else {
+            intent = new Intent(SplashActivity.this, MainActivity.class);
+
+        }
+        startActivity(intent);
+        finish();
     }
 
     @Override
@@ -68,7 +95,10 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     private void sendHandler() {
-        handler.sendEmptyMessageDelayed(0, 3000);
+        if (skip_ad.getVisibility() == View.GONE)
+            handler.sendEmptyMessageDelayed(0, 3000);
+        else
+            handler.sendEmptyMessageDelayed(1, 1000);
     }
 
 }
