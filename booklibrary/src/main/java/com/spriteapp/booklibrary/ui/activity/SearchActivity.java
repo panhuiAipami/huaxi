7699287d22ba0221374ccb13042c4ee083e5ad94
panhui <13391545091@126.com>
2018-02-01 14:50:56 +0800

    .

--- booklibrary/src/main/java/com/spriteapp/booklibrary/ui/activity/SearchActivity.java
+++ booklibrary/src/main/java/com/spriteapp/booklibrary/ui/activity/SearchActivity.java
@@ -1,12 +1,7 @@
 package com.spriteapp.booklibrary.ui.activity;
 
-import android.content.Context;
-
 import com.spriteapp.booklibrary.R;
 import com.spriteapp.booklibrary.base.BaseActivity;
-import com.spriteapp.booklibrary.listener.ChannelListener;
-import com.spriteapp.booklibrary.model.WeChatBean;
-import com.spriteapp.booklibrary.model.response.BookDetailResponse;
 
 /**
  * 搜索
@@ -26,22 +21,6 @@ public class SearchActivity extends BaseActivity {
 
     @Override
     public void configViews() throws Exception {
-        ChannelListener c = new ChannelListener() {
-            @Override
-            public void toLoginPage(Context context) {
-
-            }
-
-            @Override
-            public void showShareDialog(Context context, BookDetailResponse shareDetail, boolean isNightMode) {
-
-            }
-
-            @Override
-            public void toWXPay(WeChatBean response, String push_id) {
-
-            }
-        };
 
 
     }
