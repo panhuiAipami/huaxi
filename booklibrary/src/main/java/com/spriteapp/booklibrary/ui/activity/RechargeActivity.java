5d3d5c1930b276ae2f02dc11bf42611bf595cf0d
panhui <13391545091@126.com>
2018-02-09 18:29:57 +0800

    UI

--- booklibrary/src/main/java/com/spriteapp/booklibrary/ui/activity/RechargeActivity.java
+++ booklibrary/src/main/java/com/spriteapp/booklibrary/ui/activity/RechargeActivity.java
@@ -1,15 +1,24 @@
 package com.spriteapp.booklibrary.ui.activity;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.spriteapp.booklibrary.R;
 import com.spriteapp.booklibrary.base.BaseActivity;
+import com.spriteapp.booklibrary.model.UserBean;
+import com.spriteapp.booklibrary.util.AppUtil;
+import com.spriteapp.booklibrary.util.GlideUtils;
+import com.spriteapp.booklibrary.util.Util;
 
 
 /**
  * 充值
  */
 public class RechargeActivity extends BaseActivity implements View.OnClickListener {
+    TextView hua_ban, hua_bei, user_name;
+    ImageView user_head;
+
     @Override
     public int getLayoutResId() throws Exception {
         return R.layout.activity_recharge;
@@ -17,16 +26,19 @@ public class RechargeActivity extends BaseActivity implements View.OnClickListen
 
     @Override
     public void initData() throws Exception {
-
+        setUserData();
     }
 
     @Override
     public void configViews() throws Exception {
-
     }
 
     public void findViewId() throws Exception {
         findViewById(R.id.image_back).setOnClickListener(this);
+        user_head = (ImageView) findViewById(R.id.user_head);
+        user_name = (TextView) findViewById(R.id.user_name);
+        hua_ban = (TextView) findViewById(R.id.hua_ban);
+        hua_bei = (TextView) findViewById(R.id.hua_bei);
 
     }
 
@@ -35,6 +47,15 @@ public class RechargeActivity extends BaseActivity implements View.OnClickListen
         int i = v.getId();
         if (i == R.id.image_back) {
             finish();
+        }
+    }
+
+    public void setUserData() {
+        if (AppUtil.isLogin(this)) {
+            GlideUtils.loadImage(user_head, UserBean.getInstance().getUser_avatar(), this);
+            hua_ban.setText(Util.getString(UserBean.getInstance().getUser_false_point() + ""));
+            hua_bei.setText(Util.getString(UserBean.getInstance().getUser_real_point() + ""));
+            user_name.setText(UserBean.getInstance().getUser_nickname());
         }
     }
 }
