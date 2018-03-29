34e9d55f145490e7f0ec876abdc59ae3597d4b6d
panhui <13391545091@126.com>
2018-03-29 19:54:00 +0800

    数据库升级失败

--- booklibrary/src/main/java/com/spriteapp/booklibrary/database/BookDatabaseHelper.java
+++ booklibrary/src/main/java/com/spriteapp/booklibrary/database/BookDatabaseHelper.java
@@ -116,7 +116,7 @@ public class BookDatabaseHelper extends SQLiteOpenHelper {
         if (oldVersion == 1 && newVersion == 2) {
             String sql = "alter table [" + DbConstants.BOOK_TABLE_NAME + "] add [" + DbConstants.LAST_UPDATE_CHAPTER_TITLE + "] nvarchar(300)";
             db.execSQL(sql);
-        } else if (oldVersion == 2 && newVersion == 3) {
+        } else if (oldVersion == 2 && newVersion == 3) {//测试未发布
             //章节加 是否下载
             String sql = "alter table [" + DbConstants.CHAPTER_TABLE_NAME + "] add [" + DbConstants.CHAPTER_IS_DOWN_LOAD + "] nvarchar(300)";
             db.execSQL(sql);
@@ -124,7 +124,16 @@ public class BookDatabaseHelper extends SQLiteOpenHelper {
             //内容加 是否加密
             String sql2 = "alter table [" + DbConstants.CONTENT_TABLE_NAME + "] add [" + DbConstants.CHAPTER_ISAES + "] nvarchar(300)";
             db.execSQL(sql2);
-        } else if (oldVersion == 3 && newVersion == 4) {
+
+        } else if ((oldVersion == 2  || oldVersion == 3)&& newVersion == 4) {//其它
+            //章节加 是否下载
+            String sql = "alter table [" + DbConstants.CHAPTER_TABLE_NAME + "] add [" + DbConstants.CHAPTER_IS_DOWN_LOAD + "] nvarchar(300)";
+            db.execSQL(sql);
+
+            //内容加 是否加密
+            String sql2 = "alter table [" + DbConstants.CONTENT_TABLE_NAME + "] add [" + DbConstants.CHAPTER_ISAES + "] nvarchar(300)";
+            db.execSQL(sql2);
+
             //书架加 作者头像和名字
             String sql3 = "alter table [" + DbConstants.BOOK_TABLE_NAME + "] add [" + DbConstants.AUTHOR_AVATAR + "] nvarchar(300)";
             String sql4 = "alter table [" + DbConstants.BOOK_TABLE_NAME + "] add [" + DbConstants.AUTHOR_NAME + "] nvarchar(300)";
